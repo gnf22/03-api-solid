4ba82de33e11685c9d160e90fc0372d49ccfcbcb
gnf22 <gnferreira2000@gmail.com>
2023-06-12 00:18:49 -0300

    style: change to 2 ident spaces

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -3,21 +3,21 @@ import { FastifyRequest, FastifyReply } from 'fastify';
 import { z } from 'zod';
 
 export async function register (request: FastifyRequest, reply: FastifyReply) {
-    const registerBodySchema = z.object({
-        name: z.string(),
-        email: z.string().email(),
-        password: z.string().min(6),
-    });
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  });
 
-    const { name, email, password } = registerBodySchema.parse(request.body);
+  const { name, email, password } = registerBodySchema.parse(request.body);
 
-    await prisma.user.create({
-        data: {
-            name,
-            email,
-            password_hash: password,
-        }
-    });
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password,
+    }
+  });
 
-    return reply.status(201).send();
+  return reply.status(201).send();
 }
