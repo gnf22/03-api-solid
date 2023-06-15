6a7ef0f8cafb01b3cf270e16e7493ef8747ad5af
gnf22 <gnferreira2000@gmail.com>
2023-06-14 22:02:12 -0300

    refactor: remove unusable import

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,6 +1,5 @@
 import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository';
 import { compare } from 'bcryptjs';
-import exp from 'constants';
 import { expect, describe, it } from 'vitest';
 import { UserAlreadyExistsError } from './errors/user-already-exists-error';
 import { RegisterUseCase } from './register';
