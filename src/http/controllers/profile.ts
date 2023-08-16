0e128c49b171df333ac24f256b622fb61822fc08
gnf22 <gnferreira2000@gmail.com>
2023-08-16 18:58:14 -0300

    refactor: remove console

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -3,7 +3,5 @@ import { FastifyRequest, FastifyReply } from 'fastify';
 export async function profile (request: FastifyRequest, reply: FastifyReply) {
   await request.jwtVerify();
 
-  console.log(request.user);
-
   return reply.status(200).send();
 }
