09c519e20e41e9c6be22d9c704cd25edad40033a
gnf22 <gnferreira2000@gmail.com>
2023-08-17 18:42:45 -0300

    refactor: test invornment in prisma

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,13 +1,44 @@
+import 'dotenv/config';
+
+import { randomUUID } from 'node:crypto';
+import { execSync } from 'node:child_process';
 import { Environment } from 'vitest';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+// postgresql://docker:docker@localhost:5432/apisolid?schema=public
+
+function generateDatabaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a DATABASE_URL environment variable');
+  }
+
+  const url = new URL(process.env.DATABASE_URL);
+
+  url.searchParams.set('schema', schema);
+
+  return url.toString();
+}
 
 export default <Environment> {
   name: 'prisma',
   async setup() {
-    console.log('Setup');
+    const schema = randomUUID();
+
+    const datadatabaseURL = generateDatabaseURL(schema);
+
+    process.env.DATABASE_URL = datadatabaseURL;
+
+    execSync('npx prisma migrate deploy');
 
     return {
       async teardown() {
-        console.log('Teardown');
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`
+        );
+
+        await prisma.$disconnect();
       },
     };
   }
